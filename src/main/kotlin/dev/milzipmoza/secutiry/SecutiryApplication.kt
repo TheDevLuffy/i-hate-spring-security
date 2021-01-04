e1b43d6c4639db751049b89583346e13f3b7e2eb
thedevluffy <thedevluffy@gmail.com>
2021-01-04 22:53:20 +0900

    init project

---
+++ src/main/kotlin/dev/milzipmoza/secutiry/SecutiryApplication.kt
@@ -0,0 +1,11 @@
+package dev.milzipmoza.secutiry
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SecutiryApplication
+
+fun main(args: Array<String>) {
+	runApplication<SecutiryApplication>(*args)
+}
---
+++ src/test/kotlin/dev/milzipmoza/secutiry/SecutiryApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.milzipmoza.secutiry
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SecutiryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

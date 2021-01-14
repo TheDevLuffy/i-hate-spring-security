05f69809e6b19f686e444df5a6a159663507ad7e
thedevluffy <thedevluffy@gmail.com>
2021-01-15 00:00:58 +0900

    WebMvcConfig, SecurityConfig 구성

--- src/main/kotlin/dev/milzipmoza/secutiry/config/SecurityConfig.kt
+++ src/main/kotlin/dev/milzipmoza/secutiry/config/SecurityConfig.kt
@@ -1,11 +1,15 @@
 package dev.milzipmoza.secutiry.config
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.config.web.server.ServerHttpSecurity.http
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 @Configuration
 @EnableWebSecurity
@@ -17,6 +21,23 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity) {
         http
-                .csrf().disable()
+                .csrf {
+                    it.disable() // csrf 비활성화, 활성화 되면 Cross-Site Request Forgery 요청 난수를 포함시켜 그 난수가 없으면 동작을 거부하도록 한다.
+                }
+                .authorizeRequests {
+                    it.anyRequest().permitAll()  // 인증받은 요청은 모두 허용
+                }
+                .sessionManagement {
+                    it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 기반 인증이 아니기 때문에 비활성화 처리
+                }
+                .formLogin {
+                    it.disable() // form 로그인 역시 비활성화 처리
+                }
     }
+
+    /**
+     * 비밀번호 암호화용, 소셜 로그인이 붙으면 사용하지 않을 예정
+     */
+    @Bean
+    fun passwordEncoder(): BCryptPasswordEncoder = BCryptPasswordEncoder()
 }
---
+++ src/main/kotlin/dev/milzipmoza/secutiry/config/WebMvcConfig.kt
@@ -0,0 +1,22 @@
+package dev.milzipmoza.secutiry.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfig : WebMvcConfigurer {
+
+    /**
+     * 정적 자원 제공 클래스
+     * 사용되지 않을 예정, api 만 제공하는 서버이기 때문
+     */
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/**").addResourceLocations(*CLASSPATH_RESOURCE_LOCATIONS)
+    }
+
+    companion object {
+        private val CLASSPATH_RESOURCE_LOCATIONS = arrayOf("classpath:/static/", "classpath:/public/", "classpath:/",
+                "classpath:/resources/", "classpath:/META-INF/resources/", "classpath:/META-INF/resources/webjars/")
+    }
+}
